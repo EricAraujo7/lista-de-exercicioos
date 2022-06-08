158741d913937f859b2563db6ef4bf9613411257
Eric Ferreira <107128586+EricAraujo7@users.noreply.github.com>
2022-06-08 12:01:50 -0300

    Add files via upload

---
+++ GASTOS.java
@@ -0,0 +1,29 @@
+package exer1;
+import java.util.Scanner ;
+public class GASTOS {
+    private static Object gasto;
+    static double aluguel = 500;
+    static double alimentacao = 600;
+    static double transporte = 60;
+    static double internet = 120;
+    static double luz = 110;
+    static double agua = 80;
+    static double slario;
+
+    public class TOTAL {
+        double gasto;
+       
+}
+public static void main(String[] args){
+    
+    scanner ler = new scanner (System.in);
+    int n1, n2,soma;
+
+    gasto=(aluguel+alimentacao+transporte+internet+luz+agua);
+    slario=(gasto*2);
+    
+    System.out.println(gasto);
+    System.out.println("Salário ideal: "+(slario));
+    n1 - ler.nextinteger()
+    
+}}
